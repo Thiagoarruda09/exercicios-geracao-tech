bcfcdf5800d8c418ac6d3d513bf99d4adbcc1e35
Thiago Arruda <thiago.arruda.09@hotmail.com>
2024-10-26 08:46:15 -0300

    .
    .

--- projetos com react/main1/src/App.jsx
+++ projetos com react/main1/src/App.jsx
@@ -11,57 +11,85 @@ function App() {
   return (
     <>
       <div className="container-fluid corDeFundoBranco">
-        <div className="row d-flex flex-row">
-          <div className="col col-4 offset-1 align-self-center">
-            <p className="text-warning"><b>Melhores ofertas personalizadas</b></p>
-            <h1 >Queima de stoque Nike 🔥</h1>
-            <p >
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos quo
-              voluptatum similique, assumenda architecto velit.
-            </p>
-            <button className="btn corBotaoRosa text-light"> ver ofertas</button>
-          </div>
-          <div className="col col-6">
-            <div id="carouselExample" class="carousel slide">
-              <div class="carousel-inner">
-                <div class="carousel-item active">
-                  <img src="image.png" alt="" className="d-block w-100" />
-                </div>
-                <div class="carousel-item">
-                  <img src="image.png" alt="" className="d-block w-100" />
-                </div>
-                <div class="carousel-item">
-                  <img src="image.png" alt="" className="d-block w-100" />
+
+        <div id="carouselExample" class="carousel slide">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <div className="container">
+                <div className="row d-flex flex-row">
+                  <div className="col align-self-center">
+                    <h6 className="text-warning">Melhores ofertas personalizadas</h6>
+                    <h1>queima de estoque nike</h1>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt, quaerat.
+                    <button className="btn corBotaoRosa text-light mt-5">ver ofertas</button>
+                  </div>
+                  <div className="col">
+                    <img src="image.png" alt="" className="d-block w-100" />
+
+                  </div>
                 </div>
               </div>
-              <button
-                class="carousel-control-prev"
-                type="button"
-                data-bs-target="#carouselExample"
-                data-bs-slide="prev"
-              >
-                <span
-                  class="carousel-control-prev-icon"
-                  aria-hidden="true"
-                ></span>
-                <span class="visually-hidden">Previous</span>
-              </button>
-              <button
-                class="carousel-control-next"
-                type="button"
-                data-bs-target="#carouselExample"
-                data-bs-slide="next"
-              >
-                <span
-                  class="carousel-control-next-icon"
-                  aria-hidden="true"
-                ></span>
-                <span class="visually-hidden">Next</span>
-              </button>
+            </div>
+            <div class="carousel-item">
+              <div className="container">
+                <div className="row d-flex flex-row">
+                  <div className="col align-self-center">
+                    <h6 className="text-warning">Melhores ofertas personalizadas</h6>
+                    <h1>queima de estoque nike</h1>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt, quaerat.
+                    <button className="btn corBotaoRosa text-light mt-5">ver ofertas</button>
+                  </div>
+                  <div className="col">
+                  <img src="image.png" alt="" className="d-block w-100" />
+
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <div className="container">
+                <div className="row d-flex flex-row">
+                  <div className="col  align-self-center">
+                    <h6 className="text-warning">Melhores ofertas personalizadas</h6>
+                    <h1>queima de estoque nike</h1>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt, quaerat.
+                    <button className="btn corBotaoRosa text-light mt-5">ver ofertas</button>
+                  </div>
+                  <div className="col">
+                  <img src="image.png" alt="" className="d-block w-100" />
+
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
+          <button
+            class="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExample"
+            data-bs-slide="prev"
+          >
+            <span
+              class="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button
+            class="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExample"
+            data-bs-slide="next"
+          >
+            <span
+              class="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span class="visually-hidden">Next</span>
+          </button>
         </div>
       </div>
+
       <Componente1 />
       <br />
       <br />
@@ -72,7 +100,7 @@ function App() {
       <br />
       <br />
       <br />
-     
+
       <Componente3 />
       <br />
       <br />
@@ -80,7 +108,7 @@ function App() {
       <br />
       <br />
       <Componente4 />
-   
+
     </>
   );
 }
