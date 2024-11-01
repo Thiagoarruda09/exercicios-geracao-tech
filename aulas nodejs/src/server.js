d97115418258be8402201cab898d92d65a9e4e5a
Thiago Arruda <thiago.arruda.09@hotmail.com>
2024-11-01 12:01:19 -0300

    .
    .

--- aulas nodejs/src/server.js
+++ aulas nodejs/src/server.js
@@ -1,8 +1,8 @@
 // // importante o modulo express
-// const express = require('express')
+const express = require("express");
 
 // // app recebe express
-// const app = express()
+const app = express();
 
 // // permite que o servidor entenda json no corpo das requisições
 // app.use(express.json())
@@ -38,52 +38,118 @@
 //         "profissao":"desenvolvedor"
 //     }],)
 //   })
-//   app.get('/produtos',  (req, res)=> {
-//     res.send([{
-//         name: "sapato nike",
-//         price: 300,
-//         image: "sapato3.png",
-//       },
-//       {
-//         name: "sapato nike2",
-//         price: 400,
-//         image: "sapato3.png",
-//       },
-//       {
-//         name: "sapato nike3",
-//         price: 500,
-//         image: "sapato3.png",
-//       },
-//       {
-//         name: "sapato nike4",
-//         price: 600,
-//         image: "sapato3.png",
-//       },
-//       {
-//         name: "sapato nike5",
-//         price: 700,
-//         image: "sapato3.png",
-//       },
-//       {
-//         name: "sapato nike6",
-//         price: 800,
-//         image: "sapato3.png",
-//       },
-//       {
-//         name: "sapato nike7",
-//         price: 900,
-//         image: "sapato3.png",
-//       },
-//       {
-//         name: "sapato nike8",
-//         price: 1000,
-//         image: "sapato3.png",
-//       }])
-//   })
+app.get("/produtos", (req, res) => {
+  const produtos = req.query.produtos;
+  res.send([
+    {
+      name: "sapato nike",
+      price: 300,
+      image: "sapato3.png",
+    },
+    {
+      name: "sapato nike2",
+      price: 400,
+      image: "sapato3.png",
+    },
+    {
+      name: "sapato nike3",
+      price: 500,
+      image: "sapato3.png",
+    },
+    {
+      name: "sapato nike4",
+      price: 600,
+      image: "sapato3.png",
+    },
+    {
+      name: "sapato nike5",
+      price: 700,
+      image: "sapato3.png",
+    },
+    {
+      name: "sapato nike6",
+      price: 800,
+      image: "sapato3.png",
+    },
+    {
+      name: "sapato nike7",
+      price: 900,
+      image: "sapato3.png",
+    },
+    {
+      name: "sapato nike8",
+      price: 1000,
+      image: "sapato3.png",
+    },
+  ]);
+});
+
+app.get("/login", (req, res) => {
+  console.log('qualquer coisa')
+  const email = req.query.email;
+  const senha = req.query.senha;
+  const UserEmail = "thiago.arruda.09@hotmail.com";
+  const UserSenha = "thiago123";
+
+  const Login = (email, senha) => {
+    if (email === UserEmail && senha === UserSenha) {
+      const jwt = require("jsonwebtoken");
+      const token = jwt.sign({ id: 1, nome: "thiago" }, "asuhaoksajdiashdnjk");
+      res.send({
+        sucess: true,
+        token: token,
+        error: "",
+      });
+    } else {
+      res.send([]);
+    }
+  };
+});
+
+app.get("/Controle", (req, res) => {
+  const produtos = (token) => {
+    const jwt = require("jsonwebtoken");
+
+    const decoded = jwt.verify(token, "asuhaoksajdiashdnjk");
+    const data = [
+      {
+        nome: "sapato nike1",
+        preço: 300,
+      },
+      {
+        nome: "sapato nike2",
+        preço: 400,
+      },
+      {
+        nome: "sapato nike3",
+        preço: 500,
+      },
+    ];
+    if (decoded) {
+      return data;
+    }
+    return decoded;
+  };
+});
+
+
+
+// function login(email, senha) {
+//   const UserEmail = "thiago.arruda.09@hotmail.comsdasd";
+//   const UserSenha = "thiago123";
+
+//   if (email == UserEmail && senha == UserSenha) {
+//     const jwt = require('jsonwebtoken');
+//     const token = jwt.sign({ id: 1, name: "thiago" }, "sudgsajkcsiuchsck");
+//     return token
+//   } else {
+//     console.log("email ou senha invalidos");
+//   }
+// }
 // // subindo o servidor para a porta 3000
 
-// const PORT =3000
-// app.listen(PORT, ()=> {console.log(`o servidor está rodando na porta ${PORT}`)})
+const PORT =3000
+app.listen(PORT, ()=> {console.log(`o servidor está rodando na porta ${PORT}`)})
 
 // function login(email, senha){
 
@@ -128,45 +194,32 @@
 
 // console.log(token)
 
-function login(email, senha) {
-  const UserEmail = "thiago.arruda.09@hotmail.com";
-  const UserSenha = "thiago123";
-  const jwt = require(jsonwebtoken);
-  const token = jwt.sign({ id: 1, name: "thiago" }, "sudgsajkcsiuchsck");
+// console.log(login("thiago.arruda.09@hotmail.com", "thiago123"))
 
-  if (email == UserEmail && senha == UserSenha) {
-    console.log(token);
-  } else {
-    console.log("email ou senha invalidos");
-  }
-}
+// const produtos = (tokenUser) => {
+//   const jwt = require('jsonwebtoken');
+//   try {
+//     const decoded = jwt.verify(tokenUser, "sudgsajkcsiuchsck");
+//     const data = [
+//       {
+//         nome: "sapato nike1",
+//         preço: 300,
+//       },
+//       {
+//         nome: "sapato nike2",
+//         preço: 400,
+//       },
+//       {
+//         nome: "sapato nike3",
+//         preço: 500,
+//       },
+//     ];
+//     if (decoded) {
+//       console.log(data);
+//     }
+//     return decoded;
 
-function produtos(token) {
-  const jwtVerify = (tokenUser) => {
-    try {
-      const decoded = jwt.verify(tokenUser, "sudgsajkcsiuchsck");
-      const data = [
-        {
-          nome: "sapato nike1",
-          preço: 300,
-        },
-        {
-          nome: "sapato nike2",
-          preço: 400,
-        },
-        {
-          nome: "sapato nike3",
-          preço: 500,
-        },
-      ];
-      if (tokenUser == token) {
-        console.log(data);
-      }
-      return decoded;
-
-   
-    } catch (error) {
-      console.log(`Deu ruim! ${error}`);
-    }
-  };
-}
+//   } catch (error) {
+//     console.log(`Deu ruim! ${error}`);
+//   }
+// };
